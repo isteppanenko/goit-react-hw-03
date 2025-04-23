4ff9d66e27079ad28a7034c797e391712ea8ca34
isteppanenko <128259876+isteppanenko@users.noreply.github.com>
2025-04-23 14:22:30 +0200

    add_styles

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,42 +1,22 @@
 import PersonIcon from '@mui/icons-material/Person';
 import PhoneIcon from '@mui/icons-material/Phone';
+import styles from './Contact.module.css';
 
 const Contact = ({ contact }) => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        border: '1px solid #ccc',
-        padding: '10px',
-        borderRadius: '5px',
-        marginBottom: '10px',
-        maxWidth: '200px',
-      }}
-    >
-      <div style={{ flexGrow: 1 }}>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
+    <div className={styles.contact}>
+      <div className={styles.contactDetails}>
+        <div className={styles.contactRow}>
           <PersonIcon style={{ fontSize: 20, marginRight: '5px' }} />
-          <p style={{ margin: 0, fontWeight: 'bold' }}>{contact.name}</p>
+          <p className={styles.contactName}>{contact.name}</p>
         </div>
 
-        <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div className={styles.contactRow}>
           <PhoneIcon style={{ fontSize: 20, marginRight: '5px' }} />
-          <p style={{ margin: 0 }}>{contact.number}</p>
+          <p className={styles.contactNumber}>{contact.number}</p>
         </div>
       </div>
-      <button
-        style={{
-          backgroundColor: '#f44336',
-          color: '#fff',
-          border: 'none',
-          borderRadius: '5px',
-          padding: '5px 10px',
-          cursor: 'pointer',
-        }}
-      >
-        Delete
-      </button>
+      <button className={styles.deleteButton}>Delete</button>
     </div>
   );
 };
