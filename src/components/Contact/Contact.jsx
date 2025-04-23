abe27cd940adbcc9067d7bf10323df50a14b5fdc
isteppanenko <128259876+isteppanenko@users.noreply.github.com>
2025-04-23 13:48:45 +0200

    test_markup

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,43 @@
+import PersonIcon from '@mui/icons-material/Person';
+import PhoneIcon from '@mui/icons-material/Phone';
+
 const Contact = ({ contact }) => {
   return (
-    <>
-      <p>{contact.name}</p>
-      <p>{contact.number}</p>
-    </>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        border: '1px solid #ccc',
+        padding: '10px',
+        borderRadius: '5px',
+        marginBottom: '10px',
+        maxWidth: '200px',
+      }}
+    >
+      <div style={{ flexGrow: 1 }}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <PersonIcon style={{ fontSize: 20, marginRight: '5px' }} />
+          <p style={{ margin: 0, fontWeight: 'bold' }}>{contact.name}</p>
+        </div>
+
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <PhoneIcon style={{ fontSize: 20, marginRight: '5px' }} />
+          <p style={{ margin: 0 }}>{contact.number}</p>
+        </div>
+      </div>
+      <button
+        style={{
+          backgroundColor: '#f44336',
+          color: '#fff',
+          border: 'none',
+          borderRadius: '5px',
+          padding: '5px 10px',
+          cursor: 'pointer',
+        }}
+      >
+        Delete
+      </button>
+    </div>
   );
 };
 
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,13 @@
 import Contact from '../Contact/Contact.jsx';
+import css from './ContactList.module.css';
 
 const ContactList = ({ contactsList }) => {
   return (
-    <ul>
+    <ul style={{ padding: 0 }}>
       {contactsList.map((contact) => {
         return (
-          <li key={contact.id}>
+          <li className={css.listItem} key={contact.id}>
             <Contact contact={contact} />
-            <button>Delete</button>
           </li>
         );
       })}
