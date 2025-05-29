4a602256083e114f09f0891b51b2032a4e5b4c40
isteppanenko <128259876+isteppanenko@users.noreply.github.com>
2025-05-29 22:46:41 +0200

    validation_yup

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,12 +2,23 @@ import css from './ContactForm.module.css';
 import { useId } from 'react';
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
+import { ErrorMessage } from 'formik';
 
 const initialValues = {
   name: '',
   phone: '',
 };
 
+const validationSchem = Yup.object().shape({
+  name: Yup.string()
+    .min(3, 'Too short!')
+    .max(50, 'Too long!!!')
+    .required('Required'),
+  phone: Yup.string()
+    .min(3, 'Too short!')
+    .max(50, 'Too long!!!')
+    .required('Required'),
+});
 const ContactForm = ({ onAdd }) => {
   const nameFieldId = useId();
   const phoneFieldId = useId();
@@ -21,13 +32,19 @@ const ContactForm = ({ onAdd }) => {
   };
   return (
     <>
-      <Formik initialValues={initialValues} onSubmit={handlSubmit}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handlSubmit}
+        validationSchema={validationSchem}
+      >
         <div className={css.container}>
           <Form className={css.form}>
             <label htmlFor={nameFieldId}>Name</label>
             <Field type="text" name="name" />
+            <ErrorMessage name="name" component="span" />
             <label htmlFor={phoneFieldId}>Phone</label>
             <Field type="text" name="phone" />
+            <ErrorMessage name="phone" component="span" />
             <button type="submit" className={css.submitButton}>
               Submit
             </button>
